9118116fd7e45d9c117c4adecf6285601b402734
yanghaotai <yanghaotai@163.com>
2019-03-10 15:29:52 +0800

    19.博客阅读计数优化(2)

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.db.models.fields import exceptions
 from django.contrib.auth.models import User
 from ckeditor_uploader.fields import RichTextUploadingField
+from read_statistics.models import ReadNumExpandMethod
 
-# Create your models here.
+
 class BlogType(models.Model):
     type_name = models.CharField(max_length=15)
 
     def __str__(self):
         return self.type_name
 
-class Blog(models.Model):
+class Blog(models.Model,ReadNumExpandMethod):
     title = models.CharField(max_length=50)
     blog_type = models.ForeignKey(BlogType,on_delete=models.DO_NOTHING)
     content = RichTextUploadingField()
@@ -18,11 +18,6 @@ class Blog(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
 
-    def get_read_num(self):
-        try:
-            return self.readnum.read_num
-        except exceptions.ObjectDoesNotExist:
-            return 0
 
     def __str__(self):
         return '<Blog:%s>' %self.title
@@ -32,7 +27,3 @@ class Blog(models.Model):
         ordering = ['-created_time']
 
 
-        
-class ReadNum(models.Model):
-    read_num = models.IntegerField(default=0)
-    blog = models.OneToOneField(Blog,on_delete=models.DO_NOTHING)
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,render_to_response,get_object_or_404
 from django.core.paginator import Paginator
 from django.conf import settings
 from django.db.models import Count 
-from .models import Blog,BlogType,ReadNum    
+from .models import Blog,BlogType 
+from read_statistics.utils import read_statistics_once_read   
 
 # Create your views here.
 
@@ -75,22 +76,12 @@ def blogs_with_date(request,year,month):
 
 def blog_detail(request,blog_pk):
     blog = get_object_or_404(Blog,pk=blog_pk)
-    if not request.COOKIES.get('blog_%s_readed' %blog_pk):
-        if ReadNum.objects.filter(blog=blog).count():
-            #存在记录
-            readnum = ReadNum.objects.get(blog=blog)
-        else:
-            #不存在记录
-            readnum = ReadNum(blog=blog)
-        #计数加1
-        readnum.read_num += 1
-        readnum.save()
-
+    read_cookie_key = read_statistics_once_read(request,blog)
 
     context = {}
     context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last()
     context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
     context['blog'] = blog
     response = render_to_response('blog/blog_detail.html',context) #响应
-    response.set_cookie('blog_%s_readed' %blog_pk,'true')
+    response.set_cookie(read_cookie_key,'true') #阅读cookie标记
     return response
