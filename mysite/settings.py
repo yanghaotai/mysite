c2de985dfbcb4ba1cf9070ac5915c7214857a4bc
yanghaotai <yanghaotai@163.com>
2019-03-10 19:29:55 +0800

    21.热门阅读博客排行及缓存提速

--- mysite/settings.py
+++ mysite/settings.py
@@ -145,3 +145,11 @@ CKEDITOR_UPLOAD_PATH = 'upload/'
 
 # 自定义参数
 EACH_PAGE_BLOGS_NUMBER = 7
+
+#缓存设置
+CACHES = {
+    'default': {
+        'BACKEND':'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION':'my_cache_table',
+    }
+}
