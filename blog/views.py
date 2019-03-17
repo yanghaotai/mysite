4530b8d0babb98a2e81480b1b4a85e0732191eb8
yanghaotai <yanghaotai@163.com>
2019-03-17 21:59:30 +0800

    27.获取评论数和细节处理

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,7 @@ from django.db.models import Count
 from django.contrib.contenttypes.models import ContentType
 
 from .models import Blog,BlogType 
-from read_statistics.utils import read_statistics_once_read   
-from comment.models import Comment
-from comment.forms import CommentForm
+from read_statistics.utils import read_statistics_once_read
 
 # Create your views here.
 
@@ -81,15 +79,11 @@ def blogs_with_date(request,year,month):
 def blog_detail(request,blog_pk):
     blog = get_object_or_404(Blog,pk=blog_pk)
     read_cookie_key = read_statistics_once_read(request,blog)
-    blog_content_type = ContentType.objects.get_for_model(blog)
-    comments = Comment.objects.filter(content_type=blog_content_type,object_id=blog_pk,parent=None)
 
     context = {}
     context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last()
     context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
     context['blog'] = blog
-    context['comments'] = comments.order_by('-comment_time')
-    context['comment_form'] = CommentForm(initial={'content_type':blog_content_type.model,'object_id':blog_pk,'reply_comment_id':0})
     response = render(request,'blog/blog_detail.html',context) #响应
     response.set_cookie(read_cookie_key,'true') #阅读cookie标记
     return response
