3db6ccedc6a025e407202fb1fce9a0d3fb5e9e9e
yanghaotai <yanghaotai@163.com>
2019-03-23 14:36:30 +0800

    32.修改用户信息及绑定邮箱

--- mysite/settings.py
+++ mysite/settings.py
@@ -177,3 +177,17 @@ CACHES = {
         'LOCATION':'my_cache_table',
     }
 }
+
+# 发送邮件配置
+# https://docs.djangoproject.com/en/2.1/ref/settings/#eamil
+# https://docs.djangoproject.com/en/2.1/topics/email/
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = '596532245@qq.com'
+EMAIL_HOST_PASSWORD = 'vmwuwvtwdeedbbhc' #授权码
+EMAIL_SUBJECT_PREFIX = '[yht的博客]'
+EMAIL_USE_TLS = True #与SMTP服务器通信时，是否启动TLS链接(安全链接)
+
+
+
