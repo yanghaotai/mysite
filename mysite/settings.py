bddf348fbd6779dd8f394ce928495d59abdbea2c
yanghaotai <yanghaotai@163.com>
2019-03-25 16:16:30 +0800

    36.干净的博客-切换为MYSQL数据库(参考部署准备（二）：MySQL)

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,10 +85,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '118.24.90.64',
+        'PORT': '3306',
+        'NAME': 'mysite_db',
+        'USER': 'root',
+        'PASSWORD': 'root123456',
+        'OPTIONS': {
+            'isolation_level': None,
+            'autocommit': True,
+            'init_command': "SET sql_mode='NO_ENGINE_SUBSTITUTION,NO_AUTO_CREATE_USER'",
+        },
     }
 }
 
