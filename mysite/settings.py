20d404f364eff2910b29271a9e925c6ae759525b
yanghaotai <yanghaotai@163.com>
2020-01-07 17:04:43 +0800

    38.收集静态文件

--- mysite/settings.py
+++ mysite/settings.py
@@ -150,7 +150,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+#收集静态文件
+STATIC_ROOT = os.path.join(BASE_DIR,'static_all')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 
